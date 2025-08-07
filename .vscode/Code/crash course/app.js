0572ed77812db2974007e3a8efbba97ba710ab53
Pietro Agera <pietroagera@outlook.com>
2025-08-07 20:41:43 -0300

    policia git

---
+++ .vscode/Code/crash course/app.js
@@ -0,0 +1,9 @@
+let hasMerbership = false;
+If (hasMerbership === true)
+{
+    console.log("You can enter the club")
+} 
+else if (hasMerbership === false)
+{
+    console.log("You cannot enter the club")
+}
